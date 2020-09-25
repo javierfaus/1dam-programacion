94cdb502d536078494f5177b36caf61b426dfcd0
javierfaus <javier@enedimensiones.com>
2020-09-25 19:56:38 +0200

    Clase actualizada para hacer un pull

--- TestProject/src/GithubTestProject/ClaseTestProject.java
+++ TestProject/src/GithubTestProject/ClaseTestProject.java
@@ -4,7 +4,7 @@ public class ClaseTestProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola caracola");
+		System.out.println("Hola caracola y haz un pull desde Eclipse");
 	}
 
 }
