c14ddb948e9d3fc571fc0b81126529c1b019ea91
javie <javie@javi-x360>
2020-09-25 19:53:15 +0200

    Se añade la clase ClaseTestProject

---
+++ TestProject/src/GithubTestProject/ClaseTestProject.java
@@ -0,0 +1,10 @@
+package GithubTestProject;
+
+public class ClaseTestProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola caracola");
+	}
+
+}
